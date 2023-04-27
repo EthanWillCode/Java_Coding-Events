1be45c89fbc12578d54b5c905219052f84ca1e32
EthanWillCode <Aethil@yahoo.com>
2023-04-26 23:59:27 -0500

    Added form get and post methods

--- src/main/java/org/LaunchCode/codingevents/controllers/EventController.java
+++ src/main/java/org/LaunchCode/codingevents/controllers/EventController.java
@@ -3,22 +3,20 @@ package org.LaunchCode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Code til Dawn");
-        events.add("Coffee and Code");
-        events.add("Strange Loop");
         model.addAttribute("events",events);
         return "events/index";
     }
@@ -27,5 +25,11 @@ public class EventController {
     @RequestMapping("create")
     public String createEventForm(){
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
 }
